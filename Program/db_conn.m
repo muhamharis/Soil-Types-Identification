6a138be9590abd09015d836aaa01310928a509b9
Muhammad Haris <muhamharis@gmail.com>
2020-09-26 15:47:31 +0700

    Update db_conn.m

--- Program/db_conn.m
+++ Program/db_conn.m
@@ -2,7 +2,7 @@ function conn = db_conn()
 
 datasource = "data_skripsi_postgre";
 username = "postgres";
-password = "susahamad321";
+password = "";
 conn = database(datasource,username,password);
 
 end
